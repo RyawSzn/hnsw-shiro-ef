6210c35bd43bcd33812d306b2ce12ef34fdd10c0
RyawSzn <yamchishea@gmail.com>
2026-07-05 11:06:03 +0800

    normalize plot latency

--- final_plot.py
+++ final_plot.py
@@ -170,82 +170,57 @@ for idx, ds_name in enumerate(datasets_keys):
                 )
 
     else:
-        bl_efs_shiro = set(shiro.get(ds_name, {}).get("baseline", {}).keys())
-        bl_efs_ada = set(ada.get(ds_name, {}).get("baseline", {}).keys())
-        bl_efs = sorted(list(bl_efs_shiro.union(bl_efs_ada)))
+        # 1. First Pass: Compute the relative hardware scaling factor (Ada -> Shiro)
+        common_efs = set(shiro.get(ds_name, {}).get("baseline", {}).keys()).intersection(
+            set(ada.get(ds_name, {}).get("baseline", {}).keys())
+        )
+        
+        total_s_time = 0
+        total_a_time = 0
+        for ef in common_efs:
+            s_t = shiro[ds_name]["baseline"][ef]["times"]
+            a_t = ada[ds_name]["baseline"][ef]["times"]
+            total_s_time += s_t[1] if len(s_t) > 1 else s_t[0]
+            total_a_time += a_t[1] if len(a_t) > 1 else a_t[0]
+            
+        # Gamma scales Ada's clock speed to match Shiro's hardware environment
+        gamma = total_s_time / total_a_time if total_a_time > 0 else 1.0
+
+        # 2. Second Pass: Extract data points using Shiro as primary, falling back to scaled Ada
+        bl_efs = sorted(list(set(shiro.get(ds_name, {}).get("baseline", {}).keys()).union(
+            set(ada.get(ds_name, {}).get("baseline", {}).keys())
+        )))
 
         bl_times_med = []
         bl_avg = []
         bl_p05 = []
         bl_p01 = []
 
-        total_shiro_med = 0
-        total_ada_med = 0
-        total_unified_med = 0
-
         for ef in bl_efs:
-            s_times = (
-                shiro.get(ds_name, {}).get("baseline", {}).get(ef, {}).get("times", [])
-            )
-            a_times = (
-                ada.get(ds_name, {}).get("baseline", {}).get(ef, {}).get("times", [])
-            )
+            s_log = shiro.get(ds_name, {}).get("baseline", {}).get(ef, {})
+            a_log = ada.get(ds_name, {}).get("baseline", {}).get(ef, {})
 
-            if not s_times and not a_times:
-                continue
+            if s_log:
+                # Shiro is our baseline hardware anchor
+                times = s_log["times"]
+                med_val = times[1] if len(times) > 1 else times[0]
+                bl_times_med.append(med_val / 1000.0)
+                bl_avg.append(s_log["avg"])
+                bl_p05.append(s_log["p05"])
+                bl_p01.append(s_log["p01"])
+            elif a_log:
+                # Shiro doesn't have this ef; use Ada's recall but NORMALIZE its latency
+                times = a_log["times"]
+                med_val = times[1] if len(times) > 1 else times[0]
+                bl_times_med.append((med_val * gamma) / 1000.0) # Apply hardware correction
+                bl_avg.append(a_log["avg"])
+                bl_p05.append(a_log["p05"])
+                bl_p01.append(a_log["p01"])
 
-            if s_times and a_times:
-                s_med_val = s_times[1] if len(s_times) > 1 else s_times[0]
-                a_med_val = a_times[1] if len(a_times) > 1 else a_times[0]
-                u_med_val = (s_med_val + a_med_val) / 2.0
-                total_shiro_med += s_med_val
-                total_ada_med += a_med_val
-                total_unified_med += u_med_val
-            elif s_times:
-                s_med_val = s_times[1] if len(s_times) > 1 else s_times[0]
-                u_med_val = s_med_val
-                total_shiro_med += s_med_val
-                total_unified_med += u_med_val
-            elif a_times:
-                a_med_val = a_times[1] if len(a_times) > 1 else a_times[0]
-                u_med_val = a_med_val
-                total_ada_med += a_med_val
-                total_unified_med += u_med_val
-
-            bl_times_med.append(u_med_val / 1000.0)
-
-            target_log = shiro if s_times else ada
-            bl_avg.append(target_log[ds_name]["baseline"][ef]["avg"])
-            bl_p05.append(target_log[ds_name]["baseline"][ef]["p05"])
-            bl_p01.append(target_log[ds_name]["baseline"][ef]["p01"])
-
-        scale_shiro = total_unified_med / total_shiro_med if total_shiro_med else 1.0
-        scale_ada = total_unified_med / total_ada_med if total_ada_med else 1.0
-
-        ax.plot(
-            bl_times_med,
-            bl_avg,
-            "-",
-            color="tab:blue",
-            label=r"Baseline - Avg",
-            linewidth=2,
-        )
-        ax.plot(
-            bl_times_med,
-            bl_p05,
-            "-",
-            color="tab:green",
-            label=r"Baseline - $\pi_{0.05}$",
-            linewidth=2,
-        )
-        ax.plot(
-            bl_times_med,
-            bl_p01,
-            "-",
-            color="tab:orange",
-            label=r"Baseline - $\pi_{0.01}$",
-            linewidth=2,
-        )
+        # 3. Plot the synchronized curves
+        ax.plot(bl_times_med, bl_avg, "-", color="tab:blue", label="Baseline - Avg", linewidth=2)
+        ax.plot(bl_times_med, bl_p05, "-", color="tab:green", label=r"Baseline - $\pi_{0.05}$", linewidth=2)
+        ax.plot(bl_times_med, bl_p01, "-", color="tab:orange", label=r"Baseline - $\pi_{0.01}$", linewidth=2)
 
         if "our_method" in shiro.get(ds_name, {}):
             s_data = shiro[ds_name]["our_method"]
