ada79aa84fe9f05b6b9eaada68ef82c44d93ff89
RyawSzn <yamchishea@gmail.com>
2026-07-27 00:58:42 +0800

    msmarco building + run

--- experiments_driver/run.cpp
+++ experiments_driver/run.cpp
@@ -24,13 +24,13 @@ struct ExperimentConfig {
 
 static std::vector<ExperimentConfig> g_experiments = {
     // dataset, metric, k, alpha, gamma, expected_recall, ef_upper_bound, repeat, sampling_size, n_convergence_buckets, min_q, statics_length
-    // {"deep-image-96-angular",      "cd", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"glove-100-angular",          "cd", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"word2vec-300-angular",       "cd", 100,  0.25f, 12.0f, 0.95f, 1000,  3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"sift10m-128-euclidean",      "l2", 100,  0.25f, 12.0f, 0.95f, 1000,  3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"gist-960-euclidean",         "l2", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"tiny5m-384-euclidean",       "l2", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
-    // {"msmarco",                    "cd", 1000, 0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"deep-image-96-angular",      "cd", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"glove-100-angular",          "cd", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"word2vec-300-angular",       "cd", 100,  0.25f, 12.0f, 0.95f, 1000,  3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"sift10m-128-euclidean",      "l2", 100,  0.25f, 12.0f, 0.95f, 1000,  3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"gist-960-euclidean",         "l2", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"tiny5m-384-euclidean",       "l2", 100,  0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
+    {"msmarco",                    "cd", 1000, 0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
     // {"cohere",                     "cd", 1000, 0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
     {"cluster_mg_uniform_100d",    "cd", 1000, 0.25f, 12.0f, 0.95f, 1000, 3, 3000, 15, 3, 1 + 32 + 31 * 32},
     {"cluster_mg_zipf_100d",       "cd", 1000, 0.25f, 12.0f, 0.95f, 5000, 3, 3000, 15, 3, 1 + 32 + 31 * 32}
@@ -1728,12 +1728,12 @@ int main() {
     // insert_exp(true); // insert experiment with setup
     // delete_exp(true); // delete experiment with setup
 
-    // ablation_study_visited_list_size();      // ablation study on distance list size
-    // ablation_study_sampling_size();           // ablation study on sampling size
-    // ablation_study_weighted_decay_function(); // ablation study on weighted decay functions
-    // ablation_study_truncation_ratio();
-    // ablation_study_n_convergence_buckets();
-    // ablation_study_min_queries_per_score();        // ablation study on truncation ratio
+    ablation_study_visited_list_size();      // ablation study on distance list size
+    ablation_study_sampling_size();           // ablation study on sampling size
+    ablation_study_weighted_decay_function(); // ablation study on weighted decay functions
+    ablation_study_truncation_ratio();
+    ablation_study_n_convergence_buckets();
+    ablation_study_min_queries_per_score();        // ablation study on truncation ratio
 
     // per_query_result_exp(); // per-query result experiments
     return 0;
--- research/msmarco_add.py
+++ research/msmarco_add.py
@@ -14,7 +14,7 @@ from sklearn.datasets import make_blobs
 print("Downloaded and extracted MS MARCO V1 dataset")
 msmarco_v1_data_vecs = []
 for i in tqdm(range(0, 89)):
-    with gzip.open(str(i) + '.jsonl.gz', 'rt', encoding='utf-8') as f:
+    with gzip.open(f'/mnt/d/RyawSzn_/Downloads/msmarco-passage-openai-ada2/{i}.jsonl.gz', 'rt', encoding='utf-8') as f:
         for line in f:
             data = json.loads(line)
             msmarco_v1_data_vecs.append(data['vector'])
@@ -22,7 +22,7 @@ msmarco_v1_data_vecs = np.array(msmarco_v1_data_vecs, dtype=np.float32)
 print(msmarco_v1_data_vecs.shape)
 
 msmarco_v2_query_vecs = []
-with gzip.open('topics.msmarco-passage.dev-subset.openai-ada2.jsonl.gz', 'rt', encoding='utf-8') as f:
+with gzip.open('/mnt/d/RyawSzn_/Downloads/msmarco-passage-openai-ada2/topics.msmarco-passage.dev-subset.openai-ada2.jsonl.gz', 'rt', encoding='utf-8') as f:
     for line in f:
         data = json.loads(line)
         msmarco_v2_query_vecs.append(data['vector'])
@@ -35,10 +35,9 @@ bf_msmarco_v1.init_index(max_elements=msmarco_v1_data_vecs.shape[0])
 bf_msmarco_v1.add_items(msmarco_v1_data_vecs)
 gt_msmarco_v1, _ = bf_msmarco_v1.knn_query(msmarco_v2_query_vecs, k=1000)
 
-with h5py.File("msmarco.hdf5", "w") as h5f:
+with h5py.File("/home/ryawszn/experiments/shiro-ef/data/msmarco.hdf5", "w") as h5f:
     h5f.create_dataset("test", data=msmarco_v2_query_vecs)
     h5f.create_dataset("train", data=msmarco_v1_data_vecs)
     h5f.create_dataset("neighbors", data=gt_msmarco_v1)
 
 print("Saved MS MARCO V1 dataset and ground truth to msmarco.hdf5")
-! mv msmarco.hdf5 /home/ryawszn/experiments/shiro-ef/data/
